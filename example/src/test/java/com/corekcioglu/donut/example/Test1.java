8c7ad681de59bea2a9f33e9f3fc72896d9104c3b
Burak Çörekcioğlu <burakcorekcioglu@gmail.com>
2020-02-02 14:58:21 +0100

    Added an example test

---
+++ example/src/test/java/com/corekcioglu/donut/example/Test1.java
@@ -0,0 +1,14 @@
+package com.corekcioglu.donut.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class Test1 {
+
+    @Test
+    public void shouldPass() {
+        System.out.println("Test1.shouldPass");
+    }
+}
